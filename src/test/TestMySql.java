e59a7ff7255acaea2684562ff1eb633ee2bc4717
MarcosRaCo <marcosrabcob@gmail.com>
2021-04-21 15:45:53 +0200

    inicio

---
+++ src/test/TestMySql.java
@@ -0,0 +1,16 @@
+/**
+ * PACKAGE_NAME
+ * Nombre_project: JDBC
+ * TestMySql
+ * Created by: MarcosRa
+ * Date : 21/04/2021
+ * Description:
+ **/
+package test;
+public class TestMySql {
+    public static void main(String[] args) {
+        String userName = "root";
+        String password = "";
+        String url = "jdbc:MySQL://localhost:3306/ejercicio30";
+    }
+}
